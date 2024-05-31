2345ad192184586bb3902ab0c389b87ed9bf39a5
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-31 13:55:07 +0500

    tugadi

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-let 
+// 1-masala
+
+// for (let i = 1; i <= 50; i += 2) {
+//   console.log(i * i); 
+// }
+
+// 2-masala
+
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i * i * i); 
+//   }
+
+
+
+// // 3-masala
+
+
+
+// let p =1
+
+// for(let i =1; i<=20; i++){
+//  p=p*i
+//  console.log(p);
+// }
+
+
+// 4-masala
+
+// let sum = 0; 
+
+// for (let i = 1; i <= 30; i++) {
+//   sum += i * i;
+// }
+
+// console.log("Bir dan o'ttiz gacha bo'lgan sonlarning kvadratlari yig'indisi:", sum);
+
+// 5-masala
+
+// for(let i =1 ; i<=20; i++){
+//     let counter =0;
+// for(let j =1 ; j<=i; j++){
+//     if (i%j==0) {
+//         counter++
+//     }
+// }
+
+// if (counter==2) {
+//     console.log(i);
+// }
+
+// }
+
+
+// 6-masala
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i * 2 < 100) {
+//       console.log(i); 
+//     }
+//   }
+
+
+// 7-masala
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 === 0 && i % 5 !== 0) {
+//       console.log(i); 
+//     }
+//   }
+
+// 8-masala 
